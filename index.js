05cd2b462c33608793ea3157ba651c67816e7cc2
nazmul128 <shafayat536@gmail.com>
2022-02-14 16:00:22 +0600

    code added

--- index.js
+++ index.js
@@ -24,6 +24,42 @@ async function run(){
         const reviewCollection=database.collection('reviews');
         const productCollection=database.collection('products');
         const addUserCollection=database.collection('addUser')
+        const usersCollection=database.collection('users')
+
+        app.get('/users/:email', async(req,res)=>{
+          const email=req.params.email;
+          const query={email:email};
+          const user=await usersCollection.findOne(query);
+          let isAdmin=false;
+          if(user?.role === 'admin'){
+            isAdmin=true;
+          }
+          res.json({admin:isAdmin});
+        })
+
+        app.post('/users', async(req,res)=>{
+          const user=req.body;
+          const result=usersCollection.insertOne(user);
+          // console.log(result);
+          res.json(result);
+        });
+
+        app.put('/users', async(req,res)=>{
+          const user=req.body;
+          const filter={email:user.email};
+          const options={ upsert: true};
+          const updateDoc={ $set:user };
+          const result=await usersCollection.updateOne(filter,updateDoc,options);
+          res.json(result);
+        });
+
+        app.put('/users/admin', async(req,res)=>{
+          const user=req.body;
+          const filter={email:user.email};
+          const updateDoc={$set:{role:'admin'}};
+          const result=await usersCollection.updateOne(filter,updateDoc);
+          res.json(result);
+        });
         
         // get all items from collection
         app.get('/products', async (req, res) => {
