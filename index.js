9b87a55d558a025eff31d8e24a55fa2f9ee8df36
nazmul128 <shafayat536@gmail.com>
2022-02-14 12:16:36 +0600

    code added

---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require('express')
+const app = express()
+const { MongoClient } = require('mongodb');
+const cors=require('cors')
+const ObjectId = require("mongodb").ObjectId;
+
+
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s3ngn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri)
+
+async function run(){
+    try{
+        await client.connect();
+        const database=client.db('motor_bike');
+        const reviewCollection=database.collection('reviews');
+        const productCollection=database.collection('products');
+        const addUserCollection=database.collection('addUser')
+        
+        // get all items from collection
+        app.get('/products', async (req, res) => {
+          const cursor = productCollection.find({});
+          const items = await cursor.toArray();
+          res.send(items);
+      });
+
+
+        //get single product
+        app.get('/products/:id', async (req,res)=>{
+           const id = req.params.id;
+           const query = {_id: ObjectId(id)};
+           const result = await productCollection.findOne(query);
+           res.json(result);
+        });
+
+
+        //add a product
+        app.post('/products', async(req,res)=>{
+          const products=req.body;
+          const result=await productCollection.insertOne(products);
+          // console.log(result)
+          res.json(result);
+        });
+
+        
+
+         //reviews
+         app.post("/reviews", async (req, res) => {
+          const review=(req.body);
+          const result = await reviewCollection.insertOne(review);
+          // console.log(result)
+          res.json(result);
+        });
+
+        app.get('/reviews', async(req,res)=>{
+          const result=await reviewCollection.find({}).toArray();
+          console.log(result)
+          res.send(result);
+        })
+
+        
+         //add a user and display myOrder
+         app.post('/addUser', async(req,res)=>{
+          const addUsers=req.body;
+          const result=await addUserCollection.insertOne(addUsers);
+          // console.log(result)
+          res.json(result);
+        });
+
+        app.get('/addUser', async(req,res)=>{
+          const result=await addUserCollection.find({}).toArray();
+          // console.log(result)
+          res.send(result);
+
+        });
+
+        app.get('/addUser/:email', async(req,res)=>{
+          const email=req.params.email;
+          const query = { email: { $regex: email } };
+          const result = await addUserCollection.find(query).toArray();
+          console.log(result);
+          res.send(result);
+        });
+
+
+
+
+    }
+    finally{
+      // await client.close()
+    }
+}
+
+run().catch(console.dir)
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
