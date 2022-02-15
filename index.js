385bcf05e1c4f8f1f88ff22b67cdbebd0bcb3a90
nazmul128 <shafayat536@gmail.com>
2022-02-15 20:20:53 +0600

    code added

--- index.js
+++ index.js
@@ -126,6 +126,15 @@ async function run(){
           res.send(result);
         });
 
+           // delete product-item
+           app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+  
+
 
 
 
